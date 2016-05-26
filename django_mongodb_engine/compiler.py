5a3350896cc968ca13034fdd3e3ceaa1ad876be1
Yoshi Miyamoto <yoshi.miyamoto@voltage-ent.com>
2016-05-25 18:21:02 -0700

    use insert_many instead for bulk_create fix

--- django_mongodb_engine/compiler.py
+++ django_mongodb_engine/compiler.py
@@ -390,7 +390,7 @@ class SQLInsertCompiler(NonrelInsertCompiler, SQLCompiler):
         if return_id:
             return collection.save(doc, **options)
         else:
-            collection.insert(docs, **options)
+            collection.insert_many(docs, **options)
 
 
 # TODO: Define a common nonrel API for updates and add it to the nonrel
